6a2a57343cf69bdcfcbd22c59448a0538b85562e
est.chairat <chairat.p@kbtg.tech>
2021-09-06 17:05:28 +0700

    using mockito

--- demoapp/src/test/java/com/example/demoapp/employees/EmployeeControllerWebMvcTest.java
+++ demoapp/src/test/java/com/example/demoapp/employees/EmployeeControllerWebMvcTest.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -18,9 +20,19 @@ public class EmployeeControllerWebMvcTest {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private EmployeeService employeeService;
+
     @Test
     void getEmployeeById() throws Exception {
         int id = 100;
+
+        EmployeeResponse mockResponse = new EmployeeResponse();
+        mockResponse.setId(100);
+        mockResponse.setName("chairatz");
+
+        when(employeeService.getById(100)).thenReturn(mockResponse);
+
         MvcResult result = mvc.perform(get("/employees/" + id))
                 .andExpect(status().isOk())
                 .andReturn();
