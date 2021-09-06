36843685e9daaaf26959535ba50e23c127f5fff8
est.chairat <chairat.p@kbtg.tech>
2021-09-06 16:36:27 +0700

    add mvc test

---
+++ demoapp/src/test/java/com/example/demoapp/employees/EmployeeControllerWebMvcTest.java
@@ -0,0 +1,37 @@
+package com.example.demoapp.employees;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@WebMvcTest(EmployeeController.class)
+public class EmployeeControllerWebMvcTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void getEmployeeById() throws Exception {
+        int id = 100;
+        MvcResult result = mvc.perform(get("/employees/" + id))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        // Response body
+        byte[] json = result.getResponse().getContentAsByteArray();
+        ObjectMapper mapper = new ObjectMapper();
+        EmployeeResponse response = mapper.readValue(json, EmployeeResponse.class);
+        // Assert
+        assertEquals(id, response.getId());
+        assertEquals("chairatz", response.getName());
+
+    }
+}
