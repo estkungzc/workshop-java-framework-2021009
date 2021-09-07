5bcbeca117584f58ed58a300935d393ab8886f9e
est.chairat <chairat.p@kbtg.tech>
2021-09-07 17:05:27 +0700

    use application.properties

--- demoapp/src/test/java/com/example/demoapp/users/UserGatewayTest.java
+++ demoapp/src/test/java/com/example/demoapp/users/UserGatewayTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.boot.test.context.SpringBootTest.*;
 
@@ -15,5 +17,11 @@ class UserGatewayTest {
 
     @Test
     void getUserById() {
+        // Act
+        Optional<UserResponse> response = userGateway.getUserById(1);
+        // Assert
+        assertTrue(response.isPresent());
+        assertEquals(1, response.get().getId());
+        assertEquals("Bret", response.get().getUsername());
     }
 }
